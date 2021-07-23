a593635a07c7b690ca38860fe66708635bbc3868
Ebonie Butler <ebonie@yikesinc.com>
2021-07-22 21:02:48 -0400

    complete save functionality and styling

--- public/main.js
+++ public/main.js
@@ -11,22 +11,6 @@ const toggleAlbumEdit = (btn) => {
     readAlbum.classList.toggle('hide');
 };
 
-
-        // fetch('/albums', {
-        //     method: 'put',
-        //     headers: { 'Content-Type': 'application/json' },
-        //     body: JSON.stringify({
-        //         name: 'Born of Osiris',
-        //         album: 'Angel or Alien',
-        //     })
-        // })
-        // .then(res => {
-        //     if (res.ok) return res.json();
-        // })
-        // .then(response => {
-        //     console.log(response);
-        // });
-
 Array.from(cancelBtns).map((btn) => {
     btn.addEventListener('click', (e) => {
         const updateBtn = btn.parentElement.parentElement.querySelector('.update-button');
@@ -47,12 +31,13 @@ Array.from(updateAlbums).map((btn) => {
 });
 
 Array.from(deleteAlbums).map((album) => {
-    album.addEventListener('click', () => {
+    album.addEventListener('click', (e) => {
+        console.log(e.target.dataset.id);
         fetch('/albums', {
             method: 'delete',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                name: 'Born of Osiris',
+                'album-id': e.target.dataset.id,
             })
         })
         .then(res => {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // External dependencies.
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 
 // Tell Express that we are using the EJS template engine.
@@ -36,39 +37,42 @@ MongoClient.connect(process.env.DB_CONNECT)
     })
 
     // Album Add Endpoint.
-    app.post('/albums', (req, res) => {
+    app.post('/add-album', (req, res) => {
       console.log('send data');
+      albumsCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/');
+      })
+      .catch(error => console.error(error));
+    });
+
+    // Album Update Endpoint.
+    app.post('/update-album', (req, res) => {
       console.log(req.body);
-      albumsCollection.findOneAndUpdate(
-        { _id: 'Born' },
-        {
-          $set: {
-            name: req.body.name,
-            album: req.body.album
-          }
-        },
-        {
-          upsert: true
-        }
+      albumsCollection.updateOne(
+        { _id: ObjectId(req.body['album-id']) },
+        { $set: { 
+          "name" : req.body['album-artist'],
+          "album": req.body['album-name']
+        } }
       )
       .then(result => {
-        console.log(result);
-        res.json('Success');
+        res.redirect('/');
       })
       .catch(error => console.error(error));
     });
 
     // Album Delete Endpoint.
     app.delete('/albums', (req, res) => {
-      // Handle delete event here
+      console.log(req.body['album-id']);
       albumsCollection.deleteOne(
-        { name: req.body.name }
+        { _id: ObjectId(req.body['album-id']) }
       )
       .then(result => {
         if (result.deletedCount === 0) {
           return res.json('No albums to delete...');
         }
-        res.json(`${req.body.name} has been successfully deleted.`);
+        res.json(`Successfully deleted.`);
       })
       .catch(error => console.error(error));
     })
