444ea7870c012425691fc8e814605447541ffaf7
Ebonie Butler <ebonie@yikesinc.com>
2021-08-16 21:17:22 -0400

    login auth wip

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
+// Pull in environmental constants.
+require('dotenv').config();
+
 // External dependencies.
 const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const initPassPort = require('./passport-config');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 
+initPassPort(passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id),
+);
+
+// Tmp.
+const users = [];
+
 // Tell Express that we are using the EJS template engine.
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Pull in environmental constants.
-require('dotenv').config();
+
 
 MongoClient.connect(process.env.DB_CONNECT)
   .then(client => {
@@ -23,6 +46,40 @@ MongoClient.connect(process.env.DB_CONNECT)
     
     app.listen(3001, function() {
         console.log('listening on 3001');
+    });
+
+    // Login Endpoint.
+    app.get('/login', function(req, res) {
+      res.render('login.ejs');
+    });
+
+    app.post('/login', passport.authenticate('local',{
+      successRedirect: '/',
+      failureRedirect: 'login',
+      failureFlash: true
+    }));
+
+    // Register Endpoint.
+    app.get('/register', function(req, res) {
+      res.render('register.ejs');
+    });
+
+    app.post('/register', async function(req, res) {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        console.log(users);
+        res.redirect('/login');
+      } catch {
+        console.log('error');
+        res.redirect('/register');
+      }
+      
     });
 
     // Album Read Endpoint.
