b7eb5a52e5ddf8324de1a1735b29e2469ff70935
Ebonie Butler <ebonie@yikesinc.com>
2022-01-06 20:59:35 -0500

    prettier updates

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(flash());
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,15 +37,15 @@ MongoClient.connect(process.env.DB_CONNECT)
     const usersCollection = db.collection('users');
 
     app.use(express.static('public'));
-    app.listen(process.env.PORT, function() {
-        console.log(`listening on ${process.env.PORT}`);
+    app.listen(process.env.PORT, function () {
+      console.log(`listening on ${process.env.PORT}`);
     });
 
     initPassPort(passport,
       getUserByEmail,
       getUserById,
     );
-    
+
     async function getUserByEmail(email) {
       return await usersCollection.findOne({ email });
     }
@@ -55,28 +55,28 @@ MongoClient.connect(process.env.DB_CONNECT)
     }
 
     // Login Endpoint.
-    app.get('/login', checkNotAuthenticated, function(req, res) {
-      res.render('login.ejs');
+    app.get('/login', checkNotAuthenticated, function (req, res) {
+      res.render('login.ejs', { referer: req.headers.referer });
     });
 
-    app.post('/login', passport.authenticate('local',{
+    app.post('/login', passport.authenticate('local', {
       successRedirect: '/',
       failureRedirect: 'login',
       failureFlash: true
     }));
 
     // Logout endpoint.
-    app.post('/logout', function(req, res) {
+    app.post('/logout', function (req, res) {
       req.logOut();
       res.redirect('/login');
     });
 
     // Register Endpoint.
-    app.get('/register', checkNotAuthenticated, function(req, res) {
+    app.get('/register', checkNotAuthenticated, function (req, res) {
       res.render('register.ejs');
     });
 
-    app.post('/register', async function(req, res) {
+    app.post('/register', async function (req, res) {
 
       // Encrypt password.
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -89,36 +89,36 @@ MongoClient.connect(process.env.DB_CONNECT)
           password: hashedPassword
         }
       )
-      .then(result => {
-        res.redirect('/login');
-      })
-      .catch(error => console.error(error));
+        .then(result => {
+          res.redirect('/login');
+        })
+        .catch(error => console.error(error));
     });
 
     // Home Endpoint.
     //app.get('/', checkAuthenticated, async function(req, res) {
-    app.get('/', async function(req, res) {
+    app.get('/', async function (req, res) {
       const current = await albumsCollection.find(
         {
           status: 'none',
           is_archive: { "$exists": false }
         }
       )
-      .toArray();
+        .toArray();
       const liked = await albumsCollection.find(
         {
           status: 'liked',
           is_archive: { "$exists": false }
         },
       )
-      .toArray();
+        .toArray();
       const disliked = await albumsCollection.find(
         {
           status: 'disliked',
           is_archive: { "$exists": false }
         }
       )
-      .toArray();
+        .toArray();
 
       const archive = await albumsCollection.find(
         {
@@ -128,12 +128,10 @@ MongoClient.connect(process.env.DB_CONNECT)
           projection: { _id: 0, archive_date: 1 }
         }
       )
-      .toArray();
-      const archiveDates = [];
+        .toArray();
+      const archiveDates = {};
       archive.map(album => {
-        const obj = {};
-        obj[album.archive_date] = dateFns.format(album.archive_date, 'MMMM do, y');
-        archiveDates.push(obj);
+        archiveDates[album.archive_date] = dateFns.format(album.archive_date, 'MMMM do, y');
       });
       res.render('index.ejs', { albums: current, liked, disliked, archiveDates });
     })
@@ -147,25 +145,27 @@ MongoClient.connect(process.env.DB_CONNECT)
           status: 'none'
         }
       )
-      .then(result => {
-        res.redirect('/');
-      })
-      .catch(error => console.error(error));
+        .then(result => {
+          res.redirect('/');
+        })
+        .catch(error => console.error(error));
     });
 
     // Album Update Endpoint.
     app.post('/update-album', (req, res) => {
       albumsCollection.updateOne(
         { _id: ObjectId(req.body['album-id']) },
-        { $set: { 
-          "name" : req.body['album-artist'],
-          "album": req.body['album-name']
-        } }
+        {
+          $set: {
+            "name": req.body['album-artist'],
+            "album": req.body['album-name']
+          }
+        }
       )
-      .then(result => {
-        res.redirect('/');
-      })
-      .catch(error => console.error(error));
+        .then(result => {
+          res.redirect('/');
+        })
+        .catch(error => console.error(error));
     });
 
     // Album Delete Endpoint.
@@ -173,13 +173,13 @@ MongoClient.connect(process.env.DB_CONNECT)
       albumsCollection.deleteOne(
         { _id: ObjectId(req.body['album-id']) }
       )
-      .then(result => {
-        if (result.deletedCount === 0) {
-          return res.json('No albums to delete...');
-        }
-        res.json(`Successfully deleted.`);
-      })
-      .catch(error => console.error(error));
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No albums to delete...');
+          }
+          res.json(`Successfully deleted.`);
+        })
+        .catch(error => console.error(error));
     })
 
     // Album Post Endpoint.
@@ -188,13 +188,13 @@ MongoClient.connect(process.env.DB_CONNECT)
         { _id: ObjectId(req.body['album-id']) },
         { $set: { status: req.body['status'] } }
       )
-      .then(result => {
-        res.json(`Album status has been updated.`);
-      })
-      .catch(error => console.error(error));
+        .then(result => {
+          res.json(`Album status has been updated.`);
+        })
+        .catch(error => console.error(error));
     })
 
-     // Archive Endpoint.
+    // Archive Endpoint.
     app.get('/archive', async (req, res) => {
       try {
         const current = await albumsCollection.find(
@@ -203,21 +203,21 @@ MongoClient.connect(process.env.DB_CONNECT)
             archive_date: Number(req.query.date)
           }
         )
-        .toArray();
+          .toArray();
         const liked = await albumsCollection.find(
           {
             status: 'liked',
             archive_date: Number(req.query.date)
           },
         )
-        .toArray();
+          .toArray();
         const disliked = await albumsCollection.find(
           {
             status: 'disliked',
             archive_date: Number(req.query.date)
           }
         )
-        .toArray();
+          .toArray();
         const archive = await albumsCollection.find(
           {
             archive_date: { "$exists": true }
@@ -226,43 +226,43 @@ MongoClient.connect(process.env.DB_CONNECT)
             projection: { _id: 0, archive_date: 1 }
           }
         )
-        .toArray();
-        const archiveDates = [];
+          .toArray();
+        const archiveDates = {};
         archive.map(album => {
-          const obj = {};
-          obj[album.archive_date] = dateFns.format(album.archive_date, 'MMMM do, y');
-          archiveDates.push(obj);
+          archiveDates[album.archive_date] = dateFns.format(album.archive_date, 'MMMM do, y');
         });
-        res.render('archive.ejs', { archiveDate: dateFns.format(Number(req.query.date), 'MMMM do, y'),albums: current, liked, disliked, archiveDates });
-      } catch(e) {
+        res.render('archive.ejs', { archiveDate: dateFns.format(Number(req.query.date), 'MMMM do, y'), albums: current, liked, disliked, archiveDates });
+      } catch (e) {
         console.log(e);
       }
     });
-  
+
     app.post('/archive', async (req, res) => {
       try {
         const date = Date.now();
         const updates = await albumsCollection.updateMany(
           { is_archive: { "$exists": false } },
-          { $set: { 
-            "is_archive" : true,
-            "archive_date": date
-          } }
+          {
+            $set: {
+              "is_archive": true,
+              "archive_date": date
+            }
+          }
         );
         res.json(`Albums have been archived.`);
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
     });
   })
   .catch(error => console.error(error));
 
-  
+
 
 // Auth functions.
 function checkAuthenticated(req, res, next) {
-  if ( req.isAuthenticated()) {
-      return next();
+  if (req.isAuthenticated()) {
+    return next();
   }
 
   res.redirect('/login');
@@ -270,7 +270,7 @@ function checkAuthenticated(req, res, next) {
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-      return res.redirect('/');
+    return res.redirect('/');
   }
 
   return next();
