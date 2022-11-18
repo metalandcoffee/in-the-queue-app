c0387b89292fae9d81569f8b629ec0ca9853530d
Ebonie Butler <ebonie@yikesinc.com>
2022-11-17 20:53:30 -0500

    update default import

--- src/pages/api/delete.ts
+++ src/pages/api/delete.ts
@@ -3,27 +3,35 @@
  * Delete Endpoint.
  */
 import type { NextApiRequest, NextApiResponse } from 'next';
-import mongo from 'mongodb';
+import * as mongo from 'mongodb';
 import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 import clientPromise from '../../lib/mongodb';
 
 type Data = {
-  message: string
+	message: string;
 };
 
-export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // If artist name or album name is falsy...
-  if (!req.body.id) {
-    return res.status(400).json({ message: 'Application error. Please contact administrator.' });
-  }
+export default withApiAuthRequired(
+	async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+		// If artist name or album name is falsy...
+		if (!req.body.id) {
+			return res.status(400).json({
+				message: 'Application error. Please contact administrator.',
+			});
+		}
 
-  const client = await clientPromise;
-  const db = client.db('metal-albums');
+		const client = await clientPromise;
+		const db = client.db('metal-albums');
 
-  try {
-    await db.collection('albums').deleteOne({ _id: new mongo.ObjectId(req.body.id) });
-    return res.status(200).json({ message: 'Album deleted.' });
-  } catch (e) {
-    return res.status(500).json({ message: 'Database error. Try again later.' });
-  }
-});
+		try {
+			await db
+				.collection('albums')
+				.deleteOne({ _id: new mongo.ObjectId(req.body.id) });
+			return res.status(200).json({ message: 'Album deleted.' });
+		} catch (e) {
+			return res
+				.status(500)
+				.json({ message: 'Database error. Try again later.' });
+		}
+	}
+);
--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -3,28 +3,41 @@
  * Update Endpoint.
  */
 import type { NextApiRequest, NextApiResponse } from 'next';
-import mongo from 'mongodb';
+import * as mongo from 'mongodb';
 import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 import clientPromise from '../../lib/mongodb';
 
 type Data = {
-  message: string
+	message: string;
 };
 
-export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // If artist name or album name is falsy...
-  if (!req.body.id || !req.body.status) {
-    return res.status(400).json({ message: 'Application error. Please contact administrator.' });
-  }
+export default withApiAuthRequired(
+	async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+		// If artist name or album name is falsy...
+		console.log(req.body);
+		if (!req.body.id || !req.body.status) {
+			return res.status(400).json({
+				message: 'Application error. Please contact administrator.',
+			});
+		}
 
-  const client = await clientPromise;
-  const db = client.db('metal-albums');
+		console.log('🤘🏾');
+		const client = await clientPromise;
+		const db = client.db('metal-albums');
 
-  try {
-    await db.collection('albums').updateOne({ _id: new mongo.ObjectId(req.body.id) },
-      { $set: { status: req.body.status } });
-    return res.status(200).json({ message: 'Album updated.'});
-  } catch (e) {
-    return res.status(500).json({ message: 'Database error. Try again later.' });
-  }
-});
+		try {
+			await db
+				.collection('albums')
+				.updateOne(
+					{ _id: new mongo.ObjectId(req.body.id) },
+					{ $set: { status: req.body.status } }
+				);
+			return res.status(200).json({ message: 'Album updated.' });
+		} catch (e) {
+			console.log(e);
+			return res
+				.status(500)
+				.json({ message: 'Database error. Try again later.' });
+		}
+	}
+);
